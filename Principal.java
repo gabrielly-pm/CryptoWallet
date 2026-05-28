85b61d25f6b408d78ea896ee095fa74b74c0146a
Gabrielly <gaby.20pereiramachado@gmail.com>
2026-05-28 19:53:29 -0300

    Adiciona senha de acesso na carteira

--- Principal.java
+++ Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main (String[] args) {
-        Carteira minhaCarteira = new Carteira("Gabrielly");
+        Carteira minhaCarteira = new Carteira("Gabrielly", "1234");
         minhaCarteira.depositar(0.5);
         System.out.println("Saldo de Bitcoin: " + minhaCarteira.saldoBitcoin);
     }
